381ba9621db8cbf766890bf8ee523c3edd1118e1
ViniciusDeep <viniciuscorreia11@gmail.com>
2019-09-30 08:18:42 -0300

    [Init]Initialize project

---
+++ Revill/Revill/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Revill
+//
+//  Created by aluno on 30/09/19.
+//  Copyright © 2019 Revill. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
